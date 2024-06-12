713d8365803c00c46b79c80ec2a8b510e5376253
Zach Johnson <zachjohnson@Zachs-MBP-2.attlocal.net>
2024-06-12 11:10:54 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Contact from './Pages/CompContact.jsx'
 
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -23,26 +22,6 @@ function App() {
           <Route path='/comp_contact' exact Component={Contact}></Route>
       </Routes>
     </Router>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }
